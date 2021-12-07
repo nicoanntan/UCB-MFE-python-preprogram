64da18085d455b3612bd5dc589b1a66c794b1556
rookieyifan <fduzhangyf@outlook.com>
2021-12-08 02:52:54 +0800

    commit code

--- Homeworks/HW1/yifan_zhang/game_of_life.py
+++ Homeworks/HW1/yifan_zhang/game_of_life.py
@@ -1,7 +1,23 @@
 def evolve(initial_state):
-    # fill this out
-    pass
+    row_len = len(initial_state)
+    col_len = len(initial_state[0])
+    evolve_state = [[0 for _ in range(col_len)] for _ in range(row_len)]
 
+    for row in range(row_len):
+        for col in range(col_len):
+            neighbour_alive = 0
+            for n_row in range(max(row - 1, 0), min(row + 2, row_len)):
+                for n_col in range(max(col - 1, 0), min(col + 2, col_len)):
+                    neighbour_alive += initial_state[n_row][n_col]
+
+            if not initial_state[row][col] and neighbour_alive == 3:
+                evolve_state[row][col] = 1    # Any dead cell with three live neighbours becomes a live cell survives
+            elif initial_state[row][col] and neighbour_alive - 1 in [2, 3]:
+                evolve_state[row][col] = 1    # Any live cell with two or three live neighbours
+            else:
+                evolve_state[row][col] = 0    # All other live cells die in the next generation
+
+    return evolve_state
 
 test_case_1 = [
     [0, 0, 0, 0],
@@ -25,7 +41,6 @@ test_case_2_next = [
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
 ]
-
 
 assert evolve(test_case_1) == test_case_1
 assert evolve(test_case_2) == test_case_2_next
