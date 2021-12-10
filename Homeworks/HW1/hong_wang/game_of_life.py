82ee0f706a9e1cdd26cb3c7884f6c53a1ae40be5
bill wang <billwanghong93@gmail.com>
2021-12-10 23:59:09 +0800

    favorite movie

---
+++ Homeworks/HW1/hong_wang/game_of_life.py
@@ -0,0 +1,31 @@
+def evolve(initial_state):
+    # fill this out
+    pass
+
+
+test_case_1 = [
+    [0, 0, 0, 0],
+    [0, 1, 1, 0],
+    [0, 1, 1, 0],
+    [0, 0, 0, 0],
+]
+
+test_case_2 = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0],
+]
+
+test_case_2_next = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+]
+
+
+assert evolve(test_case_1) == test_case_1
+assert evolve(test_case_2) == test_case_2_next
